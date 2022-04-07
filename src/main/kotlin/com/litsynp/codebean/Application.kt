2b6dc8c8278dc20f418a23cf4b156fe5aba148cb
litsynp <nocte55is@gmail.com>
2022-04-07 16:28:02 +0900

    chore: rename classes

--- src/main/kotlin/com/litsynp/codebean/Application.kt
+++ src/main/kotlin/com/litsynp/codebean/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CodebeanApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<CodebeanApplication>(*args)
+    runApplication<Application>(*args)
 }
