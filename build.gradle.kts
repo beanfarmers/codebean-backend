b313f70d1e573219dde3161cff5a06ab9732270c
litsynp <nocte55is@gmail.com>
2022-04-08 17:27:09 +0900

    feat: enable Spring REST Docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.asciidoctor.convert") version "1.5.8"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
     kotlin("plugin.jpa") version "1.6.10"
@@ -17,6 +17,7 @@ repositories {
     mavenCentral()
 }
 
+val asciidoctorExtensions: Configuration by configurations.creating
 val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
@@ -36,6 +37,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
+
+    asciidoctorExtensions("org.springframework.restdocs:spring-restdocs-asciidoctor")
 }
 
 tasks.withType<KotlinCompile> {
@@ -49,11 +52,43 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+tasks.clean {
+    delete("src/main/resources/static/docs")
+}
+
 tasks.test {
+    systemProperty("org.springframework.restdocs.outputDir", snippetsDir)
     outputs.dir(snippetsDir)
 }
 
+
+tasks.build {
+    dependsOn("copyDocument")
+}
+
 tasks.asciidoctor {
-    inputs.dir(snippetsDir)
     dependsOn(tasks.test)
+
+    attributes(
+        mapOf("snippets" to snippetsDir)
+    )
+    inputs.dir(snippetsDir)
+
+    doFirst {
+        delete("src/main/resources/static/docs")
+    }
+}
+
+tasks.register("copyDocument", Copy::class) {
+    dependsOn(tasks.asciidoctor)
+    from(tasks.asciidoctor.get().outputDir)
+    into(file("src/main/resources/static/docs"))
+}
+
+tasks.bootJar {
+    dependsOn(tasks.asciidoctor)
+
+    from(tasks.asciidoctor.get().outputDir) {
+        into("BOOT-INF/classes/static/docs")
+    }
 }
